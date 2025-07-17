4c1570194fe096f73f77180268575e81d3cd84c0
ahmed <a.salawu@outlook.com>
2025-07-17 14:45:03 +0100

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/homepage"
 
 
 function App() {
 
   return (
     <>
-     
+    <Routes>
+      <Route path="/" index element={<HomePage/>} />
+    </Routes>
     </>
   )
 }
