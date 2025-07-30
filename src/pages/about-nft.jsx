c2743e0b401826351d10b64b999eb03bc1e68b24
ahmed <a.salawu@outlook.com>
2025-07-30 12:41:08 +0100

    update: updated pages

--- src/pages/about-nft.jsx
+++ src/pages/about-nft.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import banner from '../assets/artist-page-banner.png'
+
 
 function AboutNFT() {
     return (
         <div>
             <Navbar />
-            <div>
-                <div>
-                    <img src="" alt="" />
-                </div>
+            <div className='w-full h-96  relative'>
+                <img src={banner} alt="" className='w-full h-full object-cover' />
             </div>
             <Footer />
         </div>
