fd6c9ee3bee464f07340d0ae6e3615acad8dab7d
ahmed <a.salawu@outlook.com>
2025-07-30 11:34:26 +0100

    update: app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Route, Routes } from "react-router"
 import HomePage from "./pages/homepage"
+import RegisterPage from "./pages/registerpage"
+import LoginPage from "./pages/loginPage"
+import WalletPage from "./pages/walletpage"
+import ArtistPage from "./pages/artistpage"
 
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
     <>
     <Routes>
       <Route path="/" index element={<HomePage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/connect-wallet" element={<WalletPage/>} />
+      <Route path='/artist-page' element={<ArtistPage/>} />
     </Routes>
     </>
   )
