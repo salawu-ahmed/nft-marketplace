aeb3edf7ebc6f938965a03d0f83e2bd6cc40e721
ahmed <a.salawu@outlook.com>
2025-07-31 18:44:15 +0000

    refactor: replaced repeating ui elements with reusable components

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,170 +1,174 @@
 import React from 'react'
 import Navbar from '../components/navbar'
-import HeroSection from '../components/heroSection'
-import TrendingCollections from '../components/trendingCollection'
-import CreatorsSection from '../components/creatorSection'
-import Categories from '../components/categories'
-import DiscoverNFT from '../components/discoverNFTs'
-import icon from '../assets/Icon.png'
 import cta_photo from '../assets/cta_photo.png'
 import Footer from '../components/footer'
 import SectionTitle from '../components/section-title'
-import avatar from '../assets/artist_avatar.png'
+import { PiRocketLaunch } from 'react-icons/pi'
+import heroImage from '../assets/heroSection_image.png'
+import avatar from '../assets/heroSection_avatar.png'
+import Button from '../components/button'
+import CollectionsCard from '../components/collections-card'
+import ArtistCard from '../components/artist-card'
+import CategoryCard from '../components/categories-card'
+import InfoCard from '../components/info-card'
+import NFTcard from '../components/nft-card'
+import { FaRegEye } from 'react-icons/fa6'
+import Section from '../components/section'
+// import avatar from '../assets/trendingItem-avatar.png'
 
 function HomePage() {
     return (
         <div>
             <Navbar />
-            <HeroSection />
-            <TrendingCollections />
-            {/* <CreatorsSection /> */}
+
+            {/* HERO SECTION */}
+            <section className='container mx-auto h-auto flex flex-col gap-y-6 md:flex-row md:justify-between md:gap-x-12 md:gap-y-0 py-7 px-12 lg:py-14  ' >
+                <div className='w-full md:w-1/2 mb-3 md:mb-0 flex-1 '>
+                    <h1 className='text-3xl md:text-4xl lg:text-7xl mb-4'>Discover Digital Art & Collect NFTs</h1>
+                    <p className='text-l md:text-xl lg:text-2xl mb-4'>NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+                    <Button type='filled' icon={PiRocketLaunch} buttonText='Get Started' size='large' />
+                    <ul className='hidden md:flex items-center justify-between'>
+                        <li>
+                            <p className='text-xl lg:text-3xl'>240k+</p>
+                            <p>Total Sales</p>
+                        </li>
+                        <li>
+                            <p className='text-xl lg:text-3xl'>100k+</p>
+                            <p>Auctions</p>
+                        </li>
+                        <li>
+                            <p className='text-xl lg:text-3xl'>240k+</p>
+                            <p>Artists</p>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div className='w-full md:w-1/2 lg:w-2/5 rounded-xl overflow-hidden flex-1'>
+                    <div className='w-full h-4/5'>
+                        <img src={heroImage} alt="" className='w-full h-full object-cover' />
+                    </div>
+                    <div className='h-full flex flex-col bg-secondaryBackground gap-2 p-4 md:p-2 lg:p-5'>
+                        <p>Space Walking</p>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-8 h-8 rounded-full relative'>
+                                <img src={avatar} alt="" className='w-full h-full object-fill rounded-full' />
+                            </div>
+                            <p>Animakid</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* mobile view */}
+                <div className='w-full flex flex-col space-y-6 md:hidden'>
+                    <button className='w-full flex justify-center md:hidden items-center gap-2 p-3 rounded-2xl bg-cta'>
+                        <PiRocketLaunch />
+                        Get Started
+                    </button>
+                    <ul className='w-full flex gap-2 md:hidden items-center justify-between'>
+                        <li>
+                            <p className='text-xl font-semibold sm:text-xl'>240k+</p>
+                            <p className='text-xs'>Total Sales</p>
+                        </li>
+                        <li>
+                            <p className='text-xl font-semibold sm:text-xl'>100k+</p>
+                            <p className='text-xs'>Auctions</p>
+                        </li>
+                        <li>
+                            <p className='text-xl font-semibold sm:text-xl'>240k+</p>
+                            <p className='text-xs'>Artists</p>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            {/* trending collections */}
+            <Section>
+                <SectionTitle
+                    title='Trending Collections'
+                    subtitle='Checkout our weekly updated trending collections'
+                    button={false}
+                />
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    <CollectionsCard />
+                    <CollectionsCard />
+                    <CollectionsCard />
+                </div>
+            </Section>
+
+
             {/* CREATOR SECTION */}
-            <section className='container mx-auto w-full h-auto py-7 px-12 lg:py-14 lg:px-24'>
+            <Section>
                 <SectionTitle
                     title='Top Creators'
                     subtitle='Checkout Top Rated Creators On the NFT Martketplace'
+                    button={true}
+                    buttonText='See rankings'
                 />
-
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2  lg:grid-cols-4 lg:grid-rows-3 lg:gap-x-12 lg:gap-y-8">
-
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-                    <div className='bg-secondaryBackground flex lg:flex-col items-center justify-between lg:justify-center gap-4 p-4 rounded-2xl relative'>
-                        <div className='w-15 h-15 lg:w-20 lg:h-20 rounded-full overflow-hidden'>
-                            <img src={avatar} alt="" className='w-full h-full object-contain' />
-                        </div>
-                        <div className='flex flex-1 flex-col lg:items-center lg:justify-center gap-0.5'>
-                            <p>Keepitreal</p>
-                            <p className='text-xs sm:text-lg'><span className='text-label'>Total Sales:</span> 34.5 ETH</p>
-                        </div>
-                        <div className='w-6 h-6 absolute top-8 left-8 rounded-full bg-background flex items-center justify-center'>
-                            <p className='text-label'>1</p>
-                        </div>
-                    </div>
-
-
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2  lg:grid-cols-4 lg:gap-x-12 lg:gap-y-8">
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
+                    <ArtistCard />
                 </div>
-            </section>
-            <Categories />
-            <DiscoverNFT />
+            </Section>
+
+            {/* CATEGORIES SECTION */}
+            <Section>
+                <SectionTitle
+                    title='Browse Categories'
+                    subtitle='Browse NFTs across different industries'
+                />
+                <div className="w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                </div>
+            </Section>
+
+
+            {/* DISCOVER NFTs */}
+            <Section>
+                <SectionTitle
+                    title='Browse Categories'
+                    subtitle='Browse NFTs across different industries'
+                    icon={FaRegEye}
+                    buttonText='See All'
+                    button={true}
+                />
+                <div className="flex flex-col lg:flex-row justify-between gap-4">
+                    <NFTcard />
+                    <NFTcard />
+                    <NFTcard />
+                </div>
+            </Section>
+
 
             {/* HOW IT WORKS SECTION */}
-            <section className='container mx-auto w-full h-auto py-7 px-12 lg:py-14 lg:px-24'>
-                <hgroup className='mb-6'>
-                    <h1 className='text-2xl sm:text-3xl md:text-4xl mb-2'>How It Works</h1>
-                    <div>
-                        <p>Find Out How To Get Started</p>
-                    </div>
-                </hgroup>
+            <Section>
+                <SectionTitle
+                    title='How It Works'
+                    subtitle='Find Out How To Get Started'
+                    button={false}
+                />
                 <div className='flex flex-col lg:flex-row items-center gap-8'>
                     {/* card */}
-                    <div className="flex gap-4 md:flex-col items-center bg-secondaryBackground p-4 rounded-lg">
-                        <div>
-                            <img src={icon} alt="" />
-                        </div>
-                        <div>
-                            <h2 className='md:text-center'>Setup Your Wallet</h2>
-                            <p className="md:text-center">
-                                Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center bg-secondaryBackground p-4 rounded-lg">
-                        <img src={icon} alt="" />
-                        <div>
-                            <h2 className='text-center'>Setup Your Wallet</h2>
-                            <p className="text-center">
-                                Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center bg-secondaryBackground p-4 rounded-lg">
-                        <img src={icon} alt="" />
-                        <div>
-                            <h2 className='text-center'>Setup Your Wallet</h2>
-                            <p className="text-center">
-                                Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.
-                            </p>
-                        </div>
-                    </div>
-
-
+                    <InfoCard />
+                    <InfoCard />
+                    <InfoCard />
                 </div>
+            </Section>
 
-
-            </section>
-
-            <section className='container mx-auto w-full h-auto py-7 px-12 lg:py-14 lg:px-24'>
+            <Section>
                 <div className="flex flex-col md:flex-row md:bg-secondaryBackground bg-transparent p-8 md:p-16 gap-10 md:gap-20 rounded-lg">
                     <div className='w-full md:w-1/2 h-72 rounded-lg overflow-hidden'>
                         <img src={cta_photo} alt="" className='w-full h-full object-cover' />
@@ -180,7 +184,7 @@ function HomePage() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </Section>
 
             <Footer />
         </div>
